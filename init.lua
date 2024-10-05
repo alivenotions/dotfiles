35682bee6d56459eb2bd0bd79f3ad81b3165e474
Bhavdeep Dhanjal <bhavdeep@hey.com>
2024-10-05 13:54:06 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -71,10 +71,14 @@ require('lazy').setup({
       },
     },
   },
+  'justinmk/vim-sneak',
   -- create ascii diagrams
   'jbyuki/venn.nvim',
   -- ctrl-r on insert, " or @ on normal
   'junegunn/vim-peekaboo',
+
+  -- better quick fix opening
+  'yssl/QFEnter',
 
   'ntpeters/vim-better-whitespace',
 
@@ -263,7 +267,7 @@ vim.o.expandtab = true
 vim.o.smartindent = true
 -- NOTE: You should make sure your terminal supports this
 vim.o.termguicolors = true
-vim.opt["guicursor"] = ""
+vim.opt["guicursor"] = "a:blinkon0"
 
 vim.g.copilot_assume_mapped = true
 
@@ -470,23 +474,21 @@ local on_attach = function(_, bufnr)
   nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
 
   nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
+
+  nmap('gv', function(_)
+    vim.cmd([[
+      vsplit
+    ]])
+    vim.lsp.buf.definition()
+  end, '[G]oto definition in [V]split')
   nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
   nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
-  nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
   nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
   nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
 
   -- See `:help K` for why this keymap
   nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
   nmap('<leader>k', vim.lsp.buf.signature_help, 'Signature Documentation')
-
-  -- Lesser used LSP functionality
-  nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-  nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
-  nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
-  nmap('<leader>wl', function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, '[W]orkspace [L]ist Folders')
 
   -- Create a command `:Format` local to the LSP buffer
   vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
